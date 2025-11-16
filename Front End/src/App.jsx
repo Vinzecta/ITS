fafa49797bed25e8440886a3b142ae9385074a41
Lmao1232j <vinhtran23042004@gmail.com>
2025-11-16 20:48:43 +0700

    Deal git conflict

--- Front End/src/App.jsx
+++ Front End/src/App.jsx
@@ -2,23 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Homepage from "./pages/Homepage";
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
-import ForgotPassword from "./components/ForgotPassword";
 
 const App = () => {
   return (
-    // <Router>
-    //   <Header />
-    //   <Routes>
-    //     <Route path="/" element={<Homepage />} />
-    //   </Routes>
-    //   <Footer />
-    // </Router>
-    <>
-      <SignUp />
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 };
 
-export default App
+export default App;
